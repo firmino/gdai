89f05a6c21fba46e1ca1e00638cc31275420a829
firmino <firminodefaria@gmail.com>
2025-05-07 09:05:56 -0300

    task: add a docling extractor generator to avoid code repetition

--- src/parser/document_extractor.py
+++ src/parser/document_extractor.py
@@ -29,34 +29,14 @@ class DoclingPDFExtractor(DocumentExtractor):
 
     def __init__(self):
         super().__init__()
-
-        # create a DocumentConverter instance   without ocr
-        pipeline_options = PdfPipelineOptions()
-        pipeline_options.do_ocr = False  # pick what you need
-        pipeline_options.generate_page_images = True
-        pipeline_options.do_table_structure = True  # pick what you need
-        self.docling_extractor_without_ocr = DocumentConverter(
-            format_options={
-                InputFormat.PDF: PdfFormatOption(pipeline_options=pipeline_options)  # switch to beta PDF backend
-            }
-        )
-
-        # create a DocumentConverter instance   with ocr
-        pipeline_options = PdfPipelineOptions()
-        pipeline_options.do_ocr = True  # pick what you need
-        pipeline_options.generate_page_images = True
-        pipeline_options.do_table_structure = True  # pick what you need
-        self.docling_extractor_with_ocr = DocumentConverter(
-            format_options={
-                InputFormat.PDF: PdfFormatOption(pipeline_options=pipeline_options)  # switch to beta PDF backend
-            }
-        )
+        self.docling_extractor_without_ocr = self._generate_docling_pdf_extractor(use_ocr=False)
+        self.docling_extractor_with_ocr = self._generate_docling_pdf_extractor(use_ocr=True)
 
     def extract_document_data(self, document_path: str) -> dict:
         """
         Extract text from a document.
         """
-        # check if the document exists  
+        # check if the document exists
         if not os.path.exists(document_path):
             raise ValueError
 
@@ -69,16 +49,35 @@ class DoclingPDFExtractor(DocumentExtractor):
             result = self.docling_extractor_with_ocr.convert(document_path)
             dict_result = result.document.export_to_dict()
 
-        # format the output to a Document object    
+        # format the output to a Document object
         document = self._format_output(dict_result)
 
-        return document 
+        return document
+
+    def _generate_docling_pdf_extractor(self, use_ocr: bool = False) -> Document:
+        """
+        Generate a Docling PDF extractor with the specified options.
+        """
+        pipeline_options = PdfPipelineOptions()
+        pipeline_options.do_ocr = use_ocr  # pick what you need
+        pipeline_options.generate_page_images = True
+        pipeline_options.do_table_structure = True  # pick what you need
+        docling_extractor = DocumentConverter(
+            format_options={
+                InputFormat.PDF: PdfFormatOption(pipeline_options=pipeline_options)  # switch to beta PDF backend
+            }
+        )
+        return docling_extractor
 
     def _format_output(self, dict_result) -> Document:
+        """
+        Format the output to a Document object.
+        """
+ 
         # extract tables, images and text
-        tables = self._extract_tables(dict_result['tables'])
-        images = self._extract_images(dict_result['pictures'])
-        pages_text = self._extract_text(dict_result['texts'])
+        tables = self._extract_tables(dict_result["tables"])
+        images = self._extract_images(dict_result["pictures"])
+        pages_text = self._extract_text(dict_result["texts"])
 
         # create a Document object
         doc = Document(
@@ -95,16 +94,14 @@ class DoclingPDFExtractor(DocumentExtractor):
     def _extract_images(self, raw_result) -> list[Image]:
         return None
 
-    def _extract_text(self, text_result) -> list[Text]: 
+    def _extract_text(self, text_result) -> list[Text]:
         # extract text from docling result to a dictionary
-        text_found = [ (text_item['prov'][0]['page_no'], text_item['text']  ) for text_item in text_result ] 
+        text_found = [(text_item["prov"][0]["page_no"], text_item["text"]) for text_item in text_result]
 
         # group text by page number
-        page_data = defaultdict(str) 
+        page_data = defaultdict(str)
         for page_number, page_text in text_found:
             page_data[page_number] += (" " + page_text) if page_data[page_number] else page_text
-        # create a list of Page objects     
-        pages = [Text(page=page, text=text) for page, text in page_data.items()] 
-        return pages   
-
-
+        # create a list of Page objects
+        pages = [Text(page=page, text=text) for page, text in page_data.items()]
+        return pages
